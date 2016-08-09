d578e06b6385bba379626df1279d204abd314a45
w b <C:\Users\user\AppData\Roaming\The Bat!\Mail>
2016-08-09 03:02:32 +0200

    Taking a file from Ex1

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 import React from 'react'
 
+var Hello = React.createClass({
+  render: function() {
+    return <span>Hello</span>
+  }
+})
+
+var World = React.createClass({
+  render: function() {
+    return <span>world!</span>
+  }
+})
+
 export default React.createClass({
-    render: function() {
-        return <div>Hello React!</div>
-    }
+  render: function() {
+    return <div><Hello/> <World/></div>
+  }
 })
